bb94d293fac0210349657c914078e7c5cfb2cdcb
NatashaSenah <tashsenah@gmail.com>
2018-08-24 11:53:25 +0300

    add the run.py

---
+++ run.py
@@ -0,0 +1,11 @@
+class Login:
+    """
+    Class that generates new instances of login
+    """
+
+    def __init__(self,first_name,last_name,phone_number,email,password):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.phone_number = phone_number
+        self.email = email
+        self.password=password
