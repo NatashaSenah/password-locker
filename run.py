e7a75b63962c61a9dbb63dbcbdfeac3ab3038081
NatashaSenah <tashsenah@gmail.com>
2018-08-25 11:43:21 +0300

    add the created password-locker

--- run.py
+++ run.py
@@ -44,7 +44,7 @@ class Login:
         Returns :
             Boolean: True or false depending if the contact exists
         '''
-        for login in cls.login_list:
+        for login in Login.login_list:
             if login.phone_number == number:
                     return True
 
@@ -62,101 +62,5 @@ class Login:
         Function that returns all the saved login
         '''
         return Login.login_list
-    def create_login(fname,lname,phone,email,password):
-        '''
-        Function to create a new login
-        '''
-        new_login = Login(fname,lname,phone,email,password)
-        return new_login
-    def save_login(login):
-        '''
-        Function to save login
-        '''
-        login.save_login()
-    def del_login(login):
-        '''
-        Function to delete login
-        '''
-        login.delete_contact()
-    def find_login(number):
-        '''
-        Function that finds a login by number and returns the login
-        '''
-        return Login.find_by_number(number)
-    def check_existing_login(number):
-        '''
-        Function that check if a contact exists with that number and return a Boolean
-        '''
-        return Login.login_exist(number)
-
-    def display_login():
-        '''
-        Function that returns all the saved login
-        '''
-        return Login.display_login()
-    def main():
-        print("Hello welcome to twitter.")
-        user_name=input()
-        print(f"Hello{user_name}.Login")
-        print('\n')
-        while True:
-            print("Use these short codes:ca -create new account,da -display accounts,fa -find an account,pa -password,ex -exit the account")
-            short_code=input().lower()
-            if short_code=='ca':
-                print("New Account")
-                print("-"*10)
-                print("First name .....")
-                f_name=input()
-                print("Last name....")
-                l_name=input()
-                print("Phone number....")
-                p_number=input()
-                print("Email address....")
-                e_address=input()
-                print("Password....")
-                print("-"*8)
-                save_login(create_login(f_name,l_name,p_number,e_address,password))
-                print('\n')
-                print(f"New User{f_name}{l_name}created")
-                print('\n')
-            elif short_code== 'da':
-                if display_accounts():
-                    print("Twitter,Facebook,Instagram")
-                    print('\n')
-                    for account in display_accounts:
-                        print(f"{user.first_name}{user.last_name}.....{user.phone_number}")
-                        print('\n')
-                else:
-                    print('\n')
-                    print("You dont seem to have any account saved yet")
-                    print('\n')
-            elif short_code=='fa':
-                print("Enter the number you want to search for")
-                search_number=input()
-                if check_existing_user(search_number):
-                    search_user=find_user(search_number)
-                    print(f"{search_user.first_name}{search_user.last_name}")
-                    print('-' * 20)
-                    print(f"Phone number.......{search_user.phone_number}")
-                    print(f"Email address.......{search_user.email}")
-                else:
-                    print("That contact does not exist")
-            elif short_code=='pa':
-                print("Enter the password")
-                search_password=input()
-                if check_existing_user(search_password):
-                    search_user=find_user(search_password)
-                    print(f"{search_user.first_name}")
-                    print('-'*8)
-                    print(f"Phone number.....{serch_user.phone_number}")
-                    print(f"Email address....{search_user.email}")
-                else:
-                    print("That password does not exist")
-
-            elif short_code == "ex":
-                print("Bye .......")
-                break
-            else:
-                print("I really didn't get that. Please use the short codes")
-if __name__=='__main__':
-    main()
+# if __name__=='__main__':
+#     main()
---
+++ user.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python3.6
+
+from run import Login
+def create_login(fname,lname,phone,email,password):
+    '''
+    Function to create a new login
+    '''
+    new_login = Login(fname,lname,phone,email,password)
+    return new_login
+def save_login(login):
+    '''
+    Function to save login
+    '''
+    login.save_login()
+def del_login(login):
+    '''
+    Function to delete login
+    '''
+    login.delete_contact()
+def find_user(number):
+    '''
+    Function that finds a login by number and returns the login
+    '''
+    return Login.find_by_number(number)
+def check_existing_user(number):
+    '''
+    Function that check if a contact exists with that number and return a Boolean
+    '''
+    return Login.login_exist(number)
+
+def display_accounts():
+    '''
+    Function that returns all the saved login
+    '''
+    return Login.display_login()
+def main():
+    print("Hello welcome to twitter.")
+    user_name=input()
+    print(f"Hello{user_name}.Login")
+    print('\n')
+    while True:
+        print("Use these short codes:ca -create new account,da -display accounts,fa -find an account,pa -password,ex -exit the account")
+        short_code=input().lower()
+        if short_code=='ca':
+            print("New Account")
+            print("-"*10)
+            print("First name .....")
+            f_name=input()
+            print("Last name....")
+            l_name=input()
+            print("Phone number....")
+            p_number=input()
+            print("Email address....")
+            e_address=input()
+            print("Password....")
+            print("-"*8)
+            password=input()
+            save_login(create_login(f_name,l_name,p_number,e_address,password))
+            print('\n')
+            print(f"New User{f_name}{l_name}created")
+            print('\n')
+        elif short_code== 'da':
+            if display_accounts():
+                print("Twitter,Facebook,Instagram")
+                print('\n')
+                for account in Login.login_list:
+                    print(f"{account.first_name}{account.last_name}.....{account.phone_number}")
+                    print('\n')
+            else:
+                print('\n')
+                print("You dont seem to have any account saved yet")
+                print('\n')
+        elif short_code=='fa':
+            print("Enter the number you want to search for")
+            search_number=input()
+            if check_existing_user(search_number):
+                search_user=find_user(search_number)
+                print(f"{search_user.first_name}{search_user.last_name}")
+                print('-' * 20)
+                print(f"Phone number.......{search_user.phone_number}")
+                print(f"Email address.......{search_user.email}")
+            else:
+                print("That contact does not exist")
+        elif short_code=='pa':
+            print("Enter the password")
+            search_password=input()
+            if check_existing_user(search_password):
+                search_user=find_user(search_password)
+                print(f"{search_user.first_name}")
+                print('-'*8)
+                print(f"Phone number.....{serch_user.phone_number}")
+                print(f"Email address....{search_user.email}")
+            else:
+                print("That password does not exist")
+
+        elif short_code == "ex":
+            print("Bye .......")
+            break
+        else:
+            print("I really didn't get that. Please use the short codes")
+if __name__=='__main__':
+    main()
